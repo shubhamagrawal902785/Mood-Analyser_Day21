acd3c743581a79d303c0964619f87f109effdd69
shubhamagrawal902785 <shubhamag937@gmail.com>
2023-01-23 20:45:27 +0530

    Added: Analyse and respond

--- src/main/java/com/bridgelabz/com/MoodAnalyzerMain.java
+++ src/main/java/com/bridgelabz/com/MoodAnalyzerMain.java
@@ -1,2 +1,16 @@
-package com.bridgelabz.com;public class MoodAnalyzerMain {
+package com.bridgelabz.com;
+
+public class MoodAnalyzerMain {
+
+    private String message;
+    public MoodAnalyzerMain(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        if(message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
 }
--- src/test/java/com/bridgelabz/com/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/com/MoodAnalyzerTest.java
@@ -1,2 +1,23 @@
-package com.bridgelabz.com;public class MoodAnalyzerTest {
+package com.bridgelabz.com;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+    @Test
+    public void givenMessgae_WhenSad_ShouldReturn_Sad() {
+        MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("This is a Sad Message");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("SAD",mood);
+
+    }
+
+    @Test
+    public void givenMessage_WhenNotSad_ShouldReturn_Happy() {
+        MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("This is Happy Messgae");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("HAPPY",mood);
+    }
 }
+
